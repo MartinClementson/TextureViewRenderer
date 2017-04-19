e4c8e8d1ea0fec597a66fca090dbc86aa863f097
unknown <maik14@H470-0012.students.ad.bth.se>
2017-04-19 08:30:41 +0200

    Initialization

---
+++ TextureViewRenderer/main.cpp
@@ -0,0 +1,72 @@
+#include <windows.h>
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include "AntTweakBar.h"
+
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dcompiler.lib")
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+HWND InitWindow(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex = { 0 };
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.hInstance = hInstance;
+	wcex.lpszClassName = L"BTH_D3D_DEMO";
+	if (!RegisterClassEx(&wcex))
+		return false;
+
+	RECT rc = { 0, 0, 640, 480 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	HWND handle = CreateWindow(
+		L"BTH_D3D_DEMO",
+		L"BTH Direct3D Demo",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.top,
+		nullptr,
+		nullptr,
+		hInstance,
+		nullptr);
+
+	return handle;
+}
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	MSG msg = { 0 };
+	HWND wndHandle = InitWindow(hInstance); //1. Skapa fönster
+	ShowWindow(wndHandle, nCmdShow);
+	getchar();
+	getchar();
+	DestroyWindow(wndHandle);
+	_CrtDumpMemoryLeaks();
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+
+	if (TwEventWin(hWnd, message, wParam, lParam)) // send event message to AntTweakBar
+		return 0; // event has been handled by AntTweakBar
+
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
